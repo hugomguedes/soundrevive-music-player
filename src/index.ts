58aa4d1dd351a42d3ace1f41b49697f6b385996d
Alex-Guima <alexandreagtaveira@gmail.com>
2024-02-05 16:49:57 -0300

    Setup Electron + React

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { app, BrowserWindow } from 'electron';
+
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h2>Hello from React!</h2>);
+
