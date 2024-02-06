42ed3fa351fc0b0a46b5144adc5e36ecb098ecdf
Alex-Guima <alexandreagtaveira@gmail.com>
2024-02-06 13:32:21 -0300

    fix react setup again

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.body);
 root.render(<h2>Hello from React!</h2>);
+
--- src/renderer.ts
+++ src/renderer.ts
@@ -30,5 +30,4 @@ import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
-import './app';
-
+import './app'
